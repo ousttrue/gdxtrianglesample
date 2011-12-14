c40d304778ab1454852cb2214b3de0f53d6a2b49
ousttrue <ousttrue@gmail.com>
2011-12-14 11:04:33 +0900

    add desktop submodule

---
+++ desktop/src/main/java/jp/ousttrue/triangle/App.java
@@ -0,0 +1,13 @@
+package jp.ousttrue.triangle;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
