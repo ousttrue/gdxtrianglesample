e78935a91df0c77b9e0f245fb1a36706499dbf75
ousttrue <ousttrue@gmail.com>
2011-12-14 09:43:59 +0900

    add MyFirstTriangleAndroid

---
+++ android/src/main/java/jp/ousttrue/triangle/MyFirstTriangleAndroid.java
@@ -0,0 +1,13 @@
+package jp.ousttrue.triangle;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+
+public class MyFirstTriangleAndroid extends AndroidApplication {
+  @Override
+  public void onCreate (Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    initialize(new MyFirstTriangle(), false);               
+  }
+}
